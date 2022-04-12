275fb40eb70960b1146f7618fab64ba280f4f788
Drew Burt <burtd667@gmail.com>
2022-04-12 16:13:25 -0400

    added packer update file

---
+++ terraform/Update-Packer.ps1
@@ -0,0 +1,23 @@
+<#
+.Synopsis short script to update the terraform application
+.Descriptiom
+#>
+
+#get the download uri
+$URI = "https://www.packer.io/downloads"
+
+#scrape the links off the download page
+$links = (Invoke-WebRequest -Uri $URI).links.href
+
+# extract the platform download link
+($downloadLInk = $links -match "_windows_amd64")
+
+# define the  download  location
+$output = "C:\temp\packer.zip"
+
+Invoke-WebRequest -Uri "$downloadLInk" -OutFile $output
+
+#change to the source diretory
+Set-Location -Path "C:\Temp"
+
+Expand-Archive -Path .\packer.zip -DestinationPath "C:\packer" -Force
